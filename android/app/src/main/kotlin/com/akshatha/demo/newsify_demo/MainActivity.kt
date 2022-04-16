de109bb2a41956b18b87123aeadf976f0dd42a0d
Akshatha <akshatha1001@gmail.com>
2022-04-16 10:01:20 +0530

    Added my project

---
+++ android/app/src/main/kotlin/com/akshatha/demo/newsify_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akshatha.demo.newsify_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  // font family
+  static const POPPINS_BLACK = "PoppinsBlack";
+  static const POPPINS_LIGHT = "PoppinsLight";
+  static const POPPINS_SEMI_BOLD = "PoppinsSemiBold";
+}
